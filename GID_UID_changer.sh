47e8c3040cac3c6dcdd7c485c6e1a1bda78443cb
acorbe <corbisoft.codes@gmail.com>
2014-04-14 17:57:09 +0000

    first fully working test

--- GID_UID_changer.sh
+++ GID_UID_changer.sh
@@ -13,39 +13,66 @@ function is_int()
 }
 
 LOGIN=acorbe
-
-NEWUID=1001
+GROUP=acorbe
+NEWUID=1012
 NEWUID_ARG_NAME="new UID"
-NEWGID=
+NEWGID=1012
+NEWUID_ARG_NAME="new GID"
 
 OLDUID=$(id -u $LOGIN)
 OLDGID=$(id -g $GROUP)
+
+
 
 echo 'User: '$LOGIN
 echo -e '\tcurrent UID='$OLDUID
 echo -e '\tcurrent GID='$OLDGID
 
-if [ -z "$NEWUID" ]
-then
-    echo "error: destination UID is empty!"
-    exit 1
-fi
-echo -e 'checking upon new UID ('$NEWUID')...'
-is_int $NEWUID "$NEWUID_ARG_NAME"
+## UID checks
 
-RESULT=`awk -F':' -v U="$NEWUID" '$3==U {print "yes" ; exit}' $PASSWD`
+# if [ -z "$NEWUID" ]
+# then
+#     echo "error: destination UID is empty!"
+#     exit 1
+# fi
+# echo -e 'checking upon new UID ('$NEWUID')...'
+# is_int $NEWUID "$NEWUID_ARG_NAME"
 
-if [[ -z $RESULT ]]
-then
-    echo "Destination UID is available"
-else 
-    echo "Destination UID exists already. Exiting..." && exit 1
-fi
+# RESULT=`awk -F':' -v U="$NEWUID" '$3==U {print "yes" ; exit}' $PASSWD`
+
+# if [[ -z $RESULT ]]
+# then
+#     echo "Destination UID is available"
+# else 
+#     echo "Destination UID exists already. Exiting..." && exit 1
+# fi
+
+# ## GID checks
+
+# if [ -z "$NEWGID" ]
+# then
+#     echo "error: destination GID is empty!"
+#     exit 1
+# fi
+# echo -e 'checking upon new GID ('$NEWGID')...'
+# is_int $NEWGID "$NEWGID_ARG_NAME"
+
+# RESULT=`awk -F':' -v U="$NEWUID" '$4==U {print "yes" ; exit}' $PASSWD`
+
+# if [[ -z $RESULT ]]
+# then
+#     echo "Destination GID is available"
+# else 
+#     echo "Destination GID exists already. Exiting..." && exit 1
+# fi
+
 
 echo 'assigning new UID('$NEWUID') to user '$LOGIN'...'
-# usermod --uid $NEWUID $LOGIN
-# groupmod -g $NEWGID $GROUP
-# find / -user $OLDUID -exec chown -h $NEWUID {} \;
-# find / -group $OLDGID -exec chgrp -h $NEWGID {} \;
-# usermod --gid $NEWGID $LOGIN
+usermod --uid $NEWUID $LOGIN
+
+echo 'assigning new GID('$NEWGID') to user '$LOGIN'...'
+groupmod -g $NEWGID $GROUP
+find / -user $OLDUID -exec chown -h $NEWUID {} \;
+find / -group $OLDGID -exec chgrp -h $NEWGID {} \;
+usermod --gid $NEWGID $LOGIN
 
