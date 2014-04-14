c688209419cab25c77d51a6959cbdee035ca33f3
acorbe <corbisoft.codes@gmail.com>
2014-04-14 18:27:08 +0000

    UI, general structure

--- GID_UID_changer.sh
+++ GID_UID_changer.sh
@@ -2,6 +2,31 @@
 
 PASSWD=/etc/passwd
 
+NARGS=4
+
+if [ $# -ne $NARGS ]; then
+    echo error: wrong number of arguments should be $NARGS and it is $#.
+    echo usage: $0 LOGIN GROUP NEWID NEWGID
+    exit 1
+fi
+
+LOGIN=$1
+GROUP=$2
+NEWUID=$3
+NEWGID=$4
+
+## checking if by any means user is logged (e.g. via ssh)
+
+USER_LOGGED=`w -h -u $LOGIN`
+
+if [[ -z $USER_LOGGED ]]; then
+    echo 'User is not logged! Proceeding...'
+else
+    echo 'error: User is logged! Exiting...'
+    exit 1
+fi
+
+
 function is_int()
 {
     number=$1
@@ -12,11 +37,8 @@ function is_int()
     fi
 }
 
-LOGIN=acorbe
-GROUP=acorbe
-NEWUID=1012
+
 NEWUID_ARG_NAME="new UID"
-NEWGID=1012
 NEWUID_ARG_NAME="new GID"
 
 OLDUID=$(id -u $LOGIN)
@@ -30,41 +52,41 @@ echo -e '\tcurrent GID='$OLDGID
 
 ## UID checks
 
-# if [ -z "$NEWUID" ]
-# then
-#     echo "error: destination UID is empty!"
-#     exit 1
-# fi
-# echo -e 'checking upon new UID ('$NEWUID')...'
-# is_int $NEWUID "$NEWUID_ARG_NAME"
+if [ -z "$NEWUID" ]
+then
+    echo "error: destination UID is empty!"
+    exit 1
+fi
+echo -e 'checking upon new UID ('$NEWUID')...'
+is_int $NEWUID "$NEWUID_ARG_NAME"
 
-# RESULT=`awk -F':' -v U="$NEWUID" '$3==U {print "yes" ; exit}' $PASSWD`
+RESULT=`awk -F':' -v U="$NEWUID" '$3==U {print "yes" ; exit}' $PASSWD`
 
-# if [[ -z $RESULT ]]
-# then
-#     echo "Destination UID is available"
-# else 
-#     echo "Destination UID exists already. Exiting..." && exit 1
-# fi
+if [[ -z $RESULT ]]
+then
+    echo "Destination UID is available"
+else 
+    echo "Destination UID exists already. Exiting..." && exit 1
+fi
 
-# ## GID checks
+## GID checks
 
-# if [ -z "$NEWGID" ]
-# then
-#     echo "error: destination GID is empty!"
-#     exit 1
-# fi
-# echo -e 'checking upon new GID ('$NEWGID')...'
-# is_int $NEWGID "$NEWGID_ARG_NAME"
+if [ -z "$NEWGID" ]
+then
+    echo "error: destination GID is empty!"
+    exit 1
+fi
+echo -e 'checking upon new GID ('$NEWGID')...'
+is_int $NEWGID "$NEWGID_ARG_NAME"
 
-# RESULT=`awk -F':' -v U="$NEWUID" '$4==U {print "yes" ; exit}' $PASSWD`
+RESULT=`awk -F':' -v U="$NEWUID" '$4==U {print "yes" ; exit}' $PASSWD`
 
-# if [[ -z $RESULT ]]
-# then
-#     echo "Destination GID is available"
-# else 
-#     echo "Destination GID exists already. Exiting..." && exit 1
-# fi
+if [[ -z $RESULT ]]
+then
+    echo "Destination GID is available"
+else 
+    echo "Destination GID exists already. Exiting..." && exit 1
+fi
 
 
 echo 'assigning new UID('$NEWUID') to user '$LOGIN'...'
@@ -72,7 +94,13 @@ usermod --uid $NEWUID $LOGIN
 
 echo 'assigning new GID('$NEWGID') to user '$LOGIN'...'
 groupmod -g $NEWGID $GROUP
+
+echo 'changing files UID...'
 find / -user $OLDUID -exec chown -h $NEWUID {} \;
+
+echo 'changing files GID...'
 find / -group $OLDGID -exec chgrp -h $NEWGID {} \;
+
+echo 'changing '
 usermod --gid $NEWGID $LOGIN
 
